af7fdf9f6b7fe727c8f81c2ca94a9d63b52cebab
kedar joshi <kedjoshi@kedars-MacBook-Pro.local>
2019-09-22 15:57:57 -0700

    Adding PostList component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = props => (
       <header className='header'>
         <div className='header__main'>
-          <h1 className='header__logo'>reddit</h1>
+          <h1 className='header__logo'>Welcome newbees</h1>
           <h2 className="header__subreddit">r/{props.subreddit}</h2>
         </div>
         <div className="header__links">
