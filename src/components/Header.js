e2919db64464f9d93fbaaa4f52d928afa5819cee
kedar joshi <kedjoshi@kedars-MacBook-Pro.local>
2019-09-22 13:31:04 -0700

    Header stateless

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Header.css';
 
-class Header extends Component {
-  render() {
-    return (
+const Header = props => (
       <header className='header'>
         <div className='header__main'>
           <h1 className='header__logo'>reddit</h1>
-          <h2 className="header__subreddit">{this.props.subreddit}</h2>
+          <h2 className="header__subreddit">r/{props.subreddit}</h2>
         </div>
         <div className="header__links">
           <a href="/" className="header__link">log in</a>
@@ -15,7 +13,5 @@ class Header extends Component {
         </div>
       </header>
     );
-  }
-}
 
 export default Header;
