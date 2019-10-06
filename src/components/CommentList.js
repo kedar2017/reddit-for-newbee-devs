5e307223b80d3aed24e0c45912fe91c7a031cec3
kedar joshi <kedjoshi@kedars-MacBook-Pro.local>
2019-10-05 17:59:49 -0700

    Adding comment list component

---
+++ src/components/CommentList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Comment from './Comment';
+import styled from 'styled-components/macro';
+
+const List = styled.ul`
+list-style: none;
+border: 1px solid #eee;
+`;
+const Item = styled.li`
+:not(:first-child) {
+  border-top: 1px solid #eee;
+}
+`;
+
+const mapComments = comments => comments.map((comment, index) => (
+  <Item key={index} className='post-list__item'>
+    <Comment
+      score={post.score}
+      title={post.title}
+      url={post.url}
+      author={post.author}
+      timestamp={post.timestamp}
+      comments={post.comments}
+    />
+  </Item>
+));
+
+const CommentList = props => (
+  <List className='post-list'>
+    {props.comments ? mapComments(props.comments) : null}
+  </List>
+);
+
+export default CommentList;
