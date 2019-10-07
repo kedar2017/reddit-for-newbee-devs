717d7f7ace5c3f960b56b65a83a6b8f8021138a4
kedar joshi <kedjoshi@kedars-MacBook-Pro.local>
2019-10-06 17:47:39 -0700

    Modified header component with new styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,49 @@
 import React from 'react';
 import './Header.css';
 import styled from 'styled-components/macro';
+import { Link } from 'react-router-dom';
 
+const Wrapper = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 24px;
+  margin-bottom: 32px;
+  background-color: #282c34;
+`;
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const LogoLink = styled(Link)`
+  text-decoration: none;
+  font-size: 26px;
+  color: #ffffff;
+`;
+
+const Subreddit = styled.h2`
+  margin: 0 0 0 24px;
+  color: #ffffff;
+`;
+
+const NavLink = styled(Link)`
+  color: #ffffff;
+  :not(:first-child) {
+    margin-left: 16px;
+  }
+`;
 
 const Header = props => (
-      <header className='header'>
-        <div className='header__main'>
-          <h1 className='header__logo'>Welcome newbees</h1>
-          {
-            props.subreddit ? 
-            <h2 className="header__subreddit">r/{props.subreddit}</h2> : null
-          }
-          
-        </div>
-        <div className="header__links">
-          <a href="/" className="header__link">log in</a>
-          <a href="/" className="header__link">sign up</a>
-        </div>
-      </header>
-    );
+  <Wrapper>
+    <LogoWrapper>
+        <LogoLink to='/'>Welcome newbees</LogoLink>
+        {props.subreddit && <Subreddit>r/{props.subreddit}</Subreddit>}
+      <div>
+        <NavLink to='/login'>log in</NavLink>
+        <NavLink to='/signup'>sign up</NavLink>
+      </div>
+    </LogoWrapper>
+  </Wrapper>);
 
 export default Header;
