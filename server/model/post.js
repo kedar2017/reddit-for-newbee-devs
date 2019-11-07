388914f8802ecbd4b90f137d0515881e4e485499
kedar joshi <kedjoshi@kedars-MacBook-Pro.local>
2019-11-06 19:32:29 -0800

    Adding DELETE posts/:post/comments/:comment endpoint

--- server/model/post.js
+++ server/model/post.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const commentSchema = new mongoose.Schema({
+  author: { type: String, required: true },
+  body: { type: String, required: true },
+  created: { type: Date, default: Date.now }
+});
 
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
@@ -6,8 +11,30 @@ const postSchema = new mongoose.Schema({
   author: { type: String, required: true },
   category: { type: String, required: true },
   score: { type: Number, default: 0 },
+  comments: [commentSchema],
   created: { type: Date, default: Date.now }
 });
+
+postSchema.set('toJSON', { getters: true });
+
+postSchema.options.toJSON.transform = (doc, ret) => {
+  const obj = { ...ret };
+  delete obj._id;
+  delete obj.__v;
+  return obj;
+};
+
+postSchema.methods.addComment = function (author, body) {
+  this.comments.push({ author, body });
+  return this.save();
+};
+
+postSchema.methods.removeComment = function (id) {
+  const comment = this.comments.id(id);
+  if (!comment) throw new Error('Comment not found');
+  comment.remove();
+  return this.save();
+};
 
 const Post = mongoose.model('Post', postSchema);
 
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const Post = require('../models/post');
 const router = express.Router();
-router.param('id', (req, res, next, id) => {
+
+router.param('post', (req, res, next, id) => {
     Post.findById(id)
       .then(post => {
         if (!post) throw new Error('Post not found');
@@ -31,7 +32,7 @@ router.post('/', (req, res, next) => {
       .catch(next);
   });
 
-  router.delete('/:id', (req, res, next) => {
+  router.delete('/:post', (req, res, next) => {
     const { post } = req;
     post.remove()
       .then(() => res.status(204).end())
@@ -39,7 +40,7 @@ router.post('/', (req, res, next) => {
   });
   
 
-  router.post('/:id', (req, res, next) => {
+  router.post('/:post/comments', (req, res, next) => {
     const { post } = req;
     post
       .addComment(req.body.author, req.body.body)
@@ -47,4 +48,12 @@ router.post('/', (req, res, next) => {
       .catch(next);
   });
 
+  router.delete('/:post/comments/:comment', (req, res, next) => {
+    const { post } = req;
+    post
+      .removeComment(req.params.comment)
+      .then(updatedPost => res.json(updatedPost))
+      .catch(next);
+  });
+  
 module.exports = router;
