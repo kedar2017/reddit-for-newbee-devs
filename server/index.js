614a5c1b052d3316b34d447d52895865500ab41d
kedar joshi <kedjoshi@kedars-MacBook-Pro.local>
2019-11-07 21:24:35 -0800

    Adding authorization to routes, post/comment creation

--- server/index.js
+++ server/index.js
@@ -18,8 +18,7 @@ passport.use(localStrategy);
 passport.use(jwtStrategy);
 
 
-app.use('/posts', require('./routes/posts'));
-app.use(require('./routes/user'));
+require('./routes')(app);
 
 connect()
   .on('error', console.log)
---
+++ server/routes.js
@@ -0,0 +1,19 @@
+const users = require('./controllers/users');
+const posts = require('./controllers/posts');
+const comments = require('./controllers/comments');
+
+const { localAuth } = require('./auth/local');
+const { jwtAuth } = require('./auth/jwt');
+
+module.exports = app => {
+  app.post('/login', localAuth,users.login);
+  app.post('/register', users.register);
+
+  app.param('post', posts.load);
+  app.get('/posts', posts.list);
+  app.post('/posts', jwtAuth,posts.create);
+  app.delete('/posts/:post', jwtAuth, posts.destroy);
+
+  app.post('/posts/:post', jwtAuth, comments.create);
+  app.delete('/posts/:post/:comment', jwtAuth, comments.destroy);
+};
